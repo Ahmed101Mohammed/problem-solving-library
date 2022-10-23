2b43634b41291c4f357772432e4345b7a6855d26
Ahmed Mohamed 3adl <80198727+Ahmed101Mohammed@users.noreply.github.com>
2022-10-23 16:42:00 +0200

    Add main-library.js file
    This file contains all the classes from other modules in the library and collects them in one source.

---
+++ main-library.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+// modules of the library:
+
+// Array-classes modules:
+const ArrayMethods = require(path.join(__dirname, 'array-classes', 'arrayMethods.js'));
+
+// Binary-tree-classes modules:
+const BinaryTreeTraversal = require(path.join(__dirname, 'binary-tree-classes', 'binaryTreeTraversal.js'));
+
+// DataStructure modules:
+const BinaryTreeNode = require(path.join(__dirname, 'dataStructure', 'binaryTreeNode.js'));
+const HashMapForChars = require(path.join(__dirname, 'dataStructure', 'hashMapForChars.js'));
+const HashMapForNumbers = require(path.join(__dirname, 'dataStructure', 'hashMapForNumbers.js'));
+
+// Math-classes modules:
+const ComparisonsMethods = require(path.join(__dirname, 'math-classes', 'comparisonsMethods.js'));
+
+
+// module export:
+module.exports = {
+    ArrayMethods,
+    BinaryTreeTraversal,
+    BinaryTreeNode,
+    HashMapForChars,
+    HashMapForNumbers,
+    ComparisonsMethods
+}
