ff7ee252c1f996aa59e3f4a44558d2558f07f9ca
Ahmed Mohamed 3adl <80198727+Ahmed101Mohammed@users.noreply.github.com>
2022-11-01 10:50:33 +0200

    Add new class called "convertArrayToBinarryTree"
    In the new class, I built one new method called: "convertNestedArrayStructureToBinaryTree".
    The new method converts the Nested Array to a binary tree.

---
+++ array-classes/convertArrayToBinaryTree.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const BinaryTreeNode =  require(path.join(__dirname,'..', 'dataStructure', 'vinaryTreeNode.js'));
+
+class ConvertArrayToBinaryTree
+{
+    convertNestedArrayStructureToBinaryTree(array)
+    {
+        let newNode;
+        if (array === null)
+        {
+            newNode = null;
+        }
+        else if(array.constructor !== Array)
+        {
+            newNode = new BinaryTreeNode(array);
+        }
+        else if(array.constructor === Array)
+        {
+            newNode = new BinaryTreeNode(array[0]);
+            newNode.left = this.convertNestedArrayStructureToBinaryTree(array[1]);
+            newNode.right = this.convertNestedArrayStructureToBinaryTree(array[2])
+        }
+
+        return newNode;
+    }
+}
+
+module.exports  = ConvertArrayToBinaryTree;
